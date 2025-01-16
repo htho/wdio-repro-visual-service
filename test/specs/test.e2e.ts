c1774091d1c95ec824203dc9cc146b115b5621ab
Hauke T. <htho@thorenz.net>
2025-01-16 08:53:55 +0100

    reproducible screen size

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,7 +1,8 @@
 import { expect, browser, $ } from '@wdio/globals'
 
 beforeEach(async () => {
-    await browser.url(`https://htho.github.io/wdio-repro-visual-service/`)
+    await browser.url(`https://htho.github.io/wdio-repro-visual-service/`);
+    await browser.setWindowSize(1024, 768);
 });
 
 describe('Screenshot', () => {
